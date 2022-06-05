5f791c59eabeff52551ff69866865f301cd213b5
ariaramin <ariaramin24@gmail.com>
2022-06-05 20:39:46 +0430

    bottom navigation completed

--- app/src/main/java/com/ariaramin/monumentalhabits/MainActivity.kt
+++ app/src/main/java/com/ariaramin/monumentalhabits/MainActivity.kt
@@ -2,12 +2,34 @@ package com.ariaramin.monumentalhabits
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.ariaramin.monumentalhabits.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        linkBottomNavigationViewToNavHost()
+    }
+
+    private fun linkBottomNavigationViewToNavHost() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHost.navController
+        setUpBottomNavigationView(navController)
+    }
+
+    private fun setUpBottomNavigationView(navController: NavController) {
+        binding.bottomNavigationView.itemIconTintList = null
+        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/ariaramin/monumentalhabits/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/ariaramin/monumentalhabits/ui/fragments/HomeFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ariaramin.monumentalhabits.R
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class HomeFragment : Fragment() {
@@ -15,6 +17,16 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        visibleBottomAppbar()
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    private fun visibleBottomAppbar() {
+        val bottomAppBar =
+            requireActivity().findViewById<BottomAppBar>(R.id.bottomAppBar)
+        val fab =
+            requireActivity().findViewById<FloatingActionButton>(R.id.fab)
+        bottomAppBar.visibility = View.VISIBLE
+        fab.visibility = View.VISIBLE
     }
 }
